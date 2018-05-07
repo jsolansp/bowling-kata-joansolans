b441a09eb122149766004491c033096dab700ceb
Joan Solans <jsolansp@edu.tecnocampus.cat>
2018-05-07 19:44:48 +0200

    SLIDE 28

--- src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
+++ src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
@@ -16,16 +16,15 @@ public class BowlingGameTest extends TestCase {
         g = new Game();
     }
 
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++)
+            g.roll(pins);
+    }
+
     @Test
     public void testGutterGame() throws Exception {
         rollMany(20, 0);
         assertThat(g.score(),is(0));
-
-    }
-
-    private void rollMany(int n, int pins) {
-        for (int i = 0; i < n; i++)
-            g.roll(pins);
     }
 
     public void testAllOnes() throws Exception {
